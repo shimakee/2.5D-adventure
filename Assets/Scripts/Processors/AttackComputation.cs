29aed4bae7d9be9144ddb81103175c221f4404a0
shimakee <shimakee@gmail.com>
2021-03-07 01:18:18 +0800

    Added attack animation event trigger and script
    attack animation now triggers attack script which only debug.logs attack for right now.

---
+++ Assets/Scripts/Processors/AttackComputation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackComputation : MonoBehaviour
+{
+    public void Attack(AnimationEvent animationEvent)
+    {
+        float weight = animationEvent.animatorClipInfo.weight;
+
+        if(weight >= .5f)
+            Debug.Log("Attack", this);
+    }
+}
