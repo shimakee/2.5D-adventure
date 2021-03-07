324a11c0efb5f304fd15a8ea4e8d35f1b09f3005
shimakee <shimakee@gmail.com>
2021-03-07 23:33:11 +0800

    Tweeked climbing up the stairs
    climbing up stairs is now smoother.

--- Assets/Scripts/Components/Helpers/ClimbStepsComponent.cs
+++ Assets/Scripts/Components/Helpers/ClimbStepsComponent.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(ICastCollisionChecker), typeof(Rigidbody))]
 public class ClimbStepsComponent : MonoBehaviour
 {
-    [Range(0, 200)][SerializeField] float upForceToStep = 15;
+    [Range(0, 200)][SerializeField] float upForce = 15;
+    [Range(0, 200)][SerializeField] float downForce = 15;
 
     ICastCollisionChecker _castCollisionCheck;
     Rigidbody _rb;
@@ -14,10 +15,39 @@ public class ClimbStepsComponent : MonoBehaviour
         _castCollisionCheck = GetComponent<ICastCollisionChecker>();
         _rb = GetComponent<Rigidbody>();
     }
-    void Update()
+
+    private void Update()
     {
         if (!_castCollisionCheck.TopChecker && _castCollisionCheck.BottomChecker)
-            _rb.AddForce(Vector3.up * upForceToStep);
-            //_rb.velocity += Vector3.up * upForceToStep;
+        {
+            Debug.Log("Up");
+            _rb.position += Vector3.up * upForce * Time.deltaTime;
+            //_rb.AddForce(Vector3.up * upForceToStep);
+        }
+        else
+        {
+            if(!_castCollisionCheck.UnderChecker && !_castCollisionCheck.BottomChecker)
+                _rb.position += Vector3.down * downForce * Time.deltaTime;
+        }
+        //else if (_castCollisionCheck.UnderChecker)
+        //{
+        //    Debug.Log("Down");
+        //    //_rb.AddForce(Vector3.down * upForceToStep);
+
+        //    _rb.position += Vector3.down * upForce * Time.deltaTime;
+
+        //    RaycastHit hitInfo;
+        //    var isHit = Physics.Raycast(_rb.position, Vector3.down, out hitInfo, 1, mask);
+
+        //    if (isHit)
+        //    {
+        //        //_rb.position += (hitInfo.point - _rb.position);
+        //        _rb.AddForce(hitInfo.point - _rb.position);
+        //    }
+
+        //}
+        //_rb.MovePosition( _rb.position + new Vector3(0, upForceToStep, 0));
+        //_rb.velocity += Vector3.up * upForceToStep;
+
     }
 }
